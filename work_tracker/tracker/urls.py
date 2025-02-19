5ea6361a070a4b72fef43e39eed09de720038247
vachosan <vacho.vana@gmail.com>
2025-02-19 18:29:14 +0100

    edit + pridání dalšího + seznam

--- work_tracker/tracker/urls.py
+++ work_tracker/tracker/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('create/', views.create_work_record, name='create_work_record'),
     path('<int:pk>/', views.work_record_detail, name='work_record_detail'),
+    path('<int:pk>/edit/', views.edit_work_record, name='edit_work_record'),
+    path('list/', views.work_record_list, name='work_record_list'),
 ]
--- work_tracker/tracker/views.py
+++ work_tracker/tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import WorkRecordForm, PhotoDocumentationForm
 from .models import WorkRecord
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 def create_work_record(request):
     if request.method == 'POST':
@@ -31,4 +32,25 @@ def work_record_detail(request, pk):
     work_record = get_object_or_404(WorkRecord, pk=pk)
     return render(request, 'tracker/work_record_detail.html', {
         'work_record': work_record,
+    })
+
+def edit_work_record(request, pk):
+    work_record = get_object_or_404(WorkRecord, pk=pk)
+
+    if request.method == 'POST':
+        work_record_form = WorkRecordForm(request.POST, instance=work_record)
+        if work_record_form.is_valid():
+            work_record_form.save()
+            return redirect('work_record_detail', pk=work_record.pk)
+    else:
+        work_record_form = WorkRecordForm(instance=work_record)
+
+    return render(request, 'tracker/edit_work_record.html', {
+        'work_record_form': work_record_form,
+    })
+
+def work_record_list(request):
+    work_records = WorkRecord.objects.all()
+    return render(request, 'tracker/work_record_list.html', {
+        'work_records': work_records,
     })
