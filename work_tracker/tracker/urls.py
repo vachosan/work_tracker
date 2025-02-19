87e400d034dc5dcf36d2fbf125371b002b82a342
vachosan <vacho.vana@gmail.com>
2025-02-19 19:30:45 +0100

    přidávání dalších fotek přímo v detailu

--- work_tracker/tracker/urls.py
+++ work_tracker/tracker/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create/', views.create_work_record, name='create_work_record'),
     path('<int:pk>/', views.work_record_detail, name='work_record_detail'),
     path('<int:pk>/edit/', views.edit_work_record, name='edit_work_record'),
+    path('photo/<int:pk>/delete/', views.delete_photo, name='delete_photo'),
     path('list/', views.work_record_list, name='work_record_list'),
 ]
--- work_tracker/tracker/views.py
+++ work_tracker/tracker/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render, redirect
-from .forms import WorkRecordForm, PhotoDocumentationForm
-from .models import WorkRecord
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from .forms import WorkRecordForm, PhotoDocumentationForm
+from .models import WorkRecord, PhotoDocumentation
 
 def create_work_record(request):
     if request.method == 'POST':
         work_record_form = WorkRecordForm(request.POST)
         photo_form = PhotoDocumentationForm(request.POST, request.FILES)
 
-        if work_record_form.is_valid() and photo_form.is_valid():
-            # Uložení pracovního úkonu
+        if work_record_form.is_valid():
             work_record = work_record_form.save()
 
-            # Uložení fotografie s odkazem na pracovní úkon
-            photo = photo_form.save(commit=False)
-            photo.work_record = work_record
-            photo.save()
+            # Pokud je fotka vyplněna, ulož ji
+            if photo_form.is_valid() and 'photo' in request.FILES:
+                photo = photo_form.save(commit=False)
+                photo.work_record = work_record
+                photo.save()
 
             return redirect('work_record_detail', pk=work_record.pk)
     else:
@@ -30,23 +29,57 @@ def create_work_record(request):
 
 def work_record_detail(request, pk):
     work_record = get_object_or_404(WorkRecord, pk=pk)
+
+    if request.method == 'POST':
+        # Zpracování formuláře pro přidání fotky
+        photo_form = PhotoDocumentationForm(request.POST, request.FILES)
+        if photo_form.is_valid():
+            photo = photo_form.save(commit=False)
+            photo.work_record = work_record
+            photo.save()
+            return redirect('work_record_detail', pk=work_record.pk)
+    else:
+        photo_form = PhotoDocumentationForm()
+
+    # Načtení všech fotografií pro daný úkon
+    photos = work_record.photos.all()
+
     return render(request, 'tracker/work_record_detail.html', {
         'work_record': work_record,
+        'photo_form': photo_form,
+        'photos': photos,
     })
 
 def edit_work_record(request, pk):
     work_record = get_object_or_404(WorkRecord, pk=pk)
 
     if request.method == 'POST':
+        # Zpracování formuláře pro úpravu úkonu
         work_record_form = WorkRecordForm(request.POST, instance=work_record)
-        if work_record_form.is_valid():
+        photo_form = PhotoDocumentationForm(request.POST, request.FILES)
+
+        if 'save_work_record' in request.POST and work_record_form.is_valid():
             work_record_form.save()
             return redirect('work_record_detail', pk=work_record.pk)
+
+        if 'add_photo' in request.POST and photo_form.is_valid():
+            photo = photo_form.save(commit=False)
+            photo.work_record = work_record
+            photo.save()
+            return redirect('edit_work_record', pk=work_record.pk)
+
     else:
         work_record_form = WorkRecordForm(instance=work_record)
+        photo_form = PhotoDocumentationForm()
+
+    # Načtení všech fotografií pro daný úkon
+    photos = work_record.photos.all()
 
     return render(request, 'tracker/edit_work_record.html', {
         'work_record_form': work_record_form,
+        'photo_form': photo_form,
+        'photos': photos,
+        'work_record': work_record,
     })
 
 def work_record_list(request):
@@ -54,3 +87,9 @@ def work_record_list(request):
     return render(request, 'tracker/work_record_list.html', {
         'work_records': work_records,
     })
+
+def delete_photo(request, pk):
+    photo = get_object_or_404(PhotoDocumentation, pk=pk)
+    work_record_pk = photo.work_record.pk
+    photo.delete()
+    return redirect('edit_work_record', pk=work_record_pk)
