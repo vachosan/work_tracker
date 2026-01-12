841c1b3884d9c6dff4cb7baba6378ebd3a074ce4
vachosan <vacho.vana@gmail.com>
2026-01-12 08:14:45 +0100

    superuser vidí všechny projekty

--- work_tracker/tracker/permissions.py
+++ work_tracker/tracker/permissions.py
@@ -2,12 +2,16 @@ from django.shortcuts import get_object_or_404
 from .models import Project, ProjectMembership
 
 def user_projects_qs(user, roles=None):
+    if user.is_superuser:
+        return Project.objects.all()
     qs = Project.objects.filter(memberships__user=user)
     if roles:
         qs = qs.filter(memberships__role__in=roles)
     return qs.distinct()
 
 def user_can_view_project(user, project_id):
+    if user.is_superuser:
+        return True
     return ProjectMembership.objects.filter(user=user, project_id=project_id).exists()
 
 def user_is_foreman(user, project_id):
@@ -15,8 +19,10 @@ def user_is_foreman(user, project_id):
 
 def get_project_or_404_for_user(user, project_id):
     # vyhodí 404, pokud uživatel nemá k projektu přístup
+    if user.is_superuser:
+        return get_object_or_404(Project, id=project_id)
     return get_object_or_404(Project, id=project_id, memberships__user=user)
 
 def get_visible_workrecords_qs(user):
     # všechny záznamy z projektů, kam má user přístup
-    return Project.objects.filter(memberships__user=user).values_list("id", flat=True)
+    return user_projects_qs(user).values_list("id", flat=True)
